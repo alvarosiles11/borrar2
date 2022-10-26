c74cc359444e3101c8ea081eb0e056a071820645
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-10-26 15:28:34 -0400

    add Docker

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#npx prettier --check .    # Ejecuto el prettier para verificar el orden del codigo
+#npx prettier --write .    # Ejecuto el prettier para ordenar el codigo
+#NOTA permisos file.sh
+#chmod u+r+x deploy.sh
+
 docker-compose down
 git fetch
 git stash
